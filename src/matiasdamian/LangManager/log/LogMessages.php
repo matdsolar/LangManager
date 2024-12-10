fd3d79c9afc2a7454d662f78fea37c82301855fe
Matias Damian <matias@matiasdamian.com>
2024-12-10 23:58:59 +0000

    Improve logger

--- src/matiasdamian/LangManager/log/LogMessages.php
+++ src/matiasdamian/LangManager/log/LogMessages.php
@@ -6,17 +6,12 @@ namespace matiasdamian\LangManager\log;
 
 interface LogMessages{
 	
-	
-	// Parameter is not castable
 	public const PARAMETER_NOT_CASTABLE = 0;
 	
-	// No ISO message available
 	public const NO_ISO_MESSAGE = -1;
 	
-	// Fallback to default language
 	public const FALLBACK_TO_DEFAULT_LANGUAGE = -2;
 	
-	// Unsupported ISO code
-	public const UNSUPPORTED_ISO_CODE = -3;
+	public const UNSUPPORTED_ISO_CODE = -3; 
 	
 }
